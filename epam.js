c86effe4ae128040407c67416cad3fb2621b0459
neha1145 <60169293+neha1145@users.noreply.github.com>
2020-07-19 23:51:40 +0530

    Add files via upload

---
+++ epam.js
@@ -0,0 +1,9 @@
+function submit_success(){
+    alert("Registered Successfully");
+    window.open("file:///C:/Users/lenovo/OneDrive/Desktop/javascript_package/Login_epam.html",target="_self");
+}
+
+function login_success(){
+    alert("Login succesfull");
+    window.open("file:///C:/Users/lenovo/OneDrive/Desktop/javascript_package/Login_epam.html",target="_self");
+}
